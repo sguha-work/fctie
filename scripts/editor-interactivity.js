d8006b1e3cec1ec86ab1bd9a1771c55220f5b0a6
sguha-work <sahasrangshu@fusioncharts.com>
2015-10-13 13:47:34 +0530

    initial commit

---
+++ scripts/editor-interactivity.js
@@ -0,0 +1,36 @@
+window.fctie = {};
+fctie.libraryList = [
+	{
+		index: 1,
+		name: "jQuery",
+		version: "2.x",
+		url: "",
+		dependant: [],
+		isused: 0
+	},
+	{
+		index: 2,
+		name: "jQuery",
+		version: "2.x",
+		url: "",
+		dependant: [],
+		isused: 0
+	},
+	{
+		index: 3,
+		name: "jQuery UI",
+		version: "1.11.4",
+		url: "",
+		dependant: [1],
+		isused: 0
+	},
+	{
+		index: 4,
+		name: "jQuery UI",
+		version: "1.10.4",
+		url: "",
+		dependant: [1],
+		isused: 0
+	},
+];
+
